0eafd4e9589d68c66c1707ff7abc0ed2ddf9dbb3
Angelillo Davide <davide.angelillo@fabrick.com>
2024-10-29 15:58:26 +0100

    window open

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -7,10 +7,14 @@ export const Drawer: React.FC<DrawerProps> = ({
   visible,
   links
 }: DrawerProps) => {
+  function onClick(link: string): void {
+    window.open(link, 'blank')
+  }
+
   return <>
     <div className={`drawer drawer--${visible ? 'visible' : 'hidden'}`}>
       {links.map((link: Link) => 
-        <a href={link.href} aria-label={link.alt} className="drawer__link" key={link.href}>
+        <a onClick={() => onClick(link.href)} aria-label={link.alt} className="drawer__link" key={link.href}>
           <img src={link.src} alt={link.alt} className="drawer__link__image"/>
         </a>)}
     </div>
