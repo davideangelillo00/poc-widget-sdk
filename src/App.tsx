4c7be1402c9c0ed014ac109cd3a4d8cc3ab4b6f0
Angelillo Davide <davide.angelillo@fabrick.com>
2024-11-14 17:27:31 +0100

    feat: fetch data on load_sdk

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,26 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 import { Drawer, MenuButton, Profile } from './components';
 import { Link } from './components/drawer/schema/link';
-import { useFetchData } from './services/api';
+import { fetchData } from './services/api';
 
 function App() {
   const [isOpened, setOpened] = useState(false);
-  const data = useFetchData('https://catfact.ninja/fact');
+  const [data, setData] = useState();
 
   const links: Link[] = [
     { href: 'http://localhost:4201', src: 'https://www.fabrick.com/images/favicon.png', alt: 'App 1' },
     { href: 'http://localhost:4202', src: 'https://cdn.logojoy.com/wp-content/uploads/20220329171724/social-media-app-logo-instagram.jpg', alt: 'App 2' },
     { href: 'http://localhost:4203', src: 'https://cdn.logojoy.com/wp-content/uploads/20220329171710/telegram-app-logo.png', alt: 'App 3' },
   ];
+
+  useEffect(() => {
+    window.addEventListener('load_sdk', () => {
+      fetchData('https://catfact.ninja/fact').then((result) => {
+        setData(result);
+        window.dispatchEvent(new CustomEvent('sdk_loaded'));
+      })
+    })
+  }, [])
 
   useEffect(() => {
     console.log('[SDK Logger] - SDK made an HTTP call on load. Check the network tab', data);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,28 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-
-export const useFetchData = (url: string) => {
-  const [data, setData] = useState(null);
-  const hasFetched = useRef(false);
-
-  useEffect(() => {
-    if (hasFetched.current) {
-      return;
-    }
-    hasFetched.current = true;
-    (async () => {
-      try {
-        const response: Response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const result = await response.json();
-        setData(result);
-      } catch (err) {
-        console.error(err);
-      } 
-    })();
-  }, [url])
-  
-
-  return data;
+export async function fetchData(url: string): Promise<any> {
+  const response: Response = await fetch(url);
+  const result = await response.json();
+  return result;
 }
